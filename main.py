715d627caf9c5722a8516a5f73abe0457fa32aa3
yannis-mlgrn <yannismalgorn@gmail.com>
2020-10-27 13:41:18 +0100

    V0.1

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 from time import sleep
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+from random import *
 
 WEBDRIVER_PATH= 'bin/chromedriver'
 driver = webdriver.Chrome(executable_path=WEBDRIVER_PATH)
@@ -24,7 +23,7 @@ password.send_keys("your password")
 submit = driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]")
 submit.click()
 
-sleep(3)
+sleep(6)
 print("go to home")
 #enregistrer le mot de passe ?
 home = driver.find_element_by_xpath("//button[contains(text(), 'Plus tard')]")
@@ -36,10 +35,29 @@ notif = driver.find_element_by_xpath("//button[contains(text(), 'Plus tard')]")
 notif.click()
 
 sleep(3)
+nbfollow = 0
 
-for i in range(5) :
+while True :
 
-	follow = driver.find_element_by_xpath("/html/body/div[1]/section/main/section/div[3]/div[2]/div[2]/div/div/div/div[1]/div[3]/button/div")
-	follow.click()
-	sleep(1)
-	driver.refresh()
+	print("bot start")
+
+	for i in range(5) :
+		nbfollow =int(nbfollow)
+		follow = driver.find_element_by_xpath("/html/body/div[1]/section/main/section/div[3]/div[2]/div[2]/div/div/div/div[1]/div[3]/button/div")
+		#compte = driver.find_element_by_xpath("/html/body/div[1]/section/main/section/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[1]/div/span/a")
+		#str(compte)
+		follow.click()
+		sleep(1)
+		nbfollow = nbfollow + 1
+		nbfollow = str(nbfollow)
+		print("["+ nbfollow+ "]")
+		print("le bot a follow " + nbfollow + " personnes")
+		driver.refresh()
+
+	pause = randrange(20,120)
+	pause = str(pause)
+	print("pause : " + pause + "s")
+	pause = int(pause)
+	sleep(pause)
+
+
